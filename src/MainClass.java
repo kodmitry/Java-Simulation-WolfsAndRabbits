2afe0892ee1644edc730760053e1864eb9389967
Ko Dmitry <ruinmath@bk.ru>
2020-12-30 19:53:43 +0500

    First commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("This is the beginning");
+        System.out.println("Type anything to close this window");
+        new Scanner(System.in).next(); // Hold the console form closing
     }
 }
