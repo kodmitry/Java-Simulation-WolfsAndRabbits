cab827758a42fec5a3529e97e2bf2f3448ce83ab
Ko Dmitry <ruinmath@bk.ru>
2021-01-06 21:42:14 +0500

    Removed few unnecessary lines Replaced QueueAddRabbits, etc with AddAnimalsRandom that now uses enum TypeOfAnimal to determine which animals to spawn

--- src/application/logic/SimulationHandler.java
+++ src/application/logic/SimulationHandler.java
@@ -2,7 +2,6 @@ package application.logic;
 import application.logic.animals.*;
 import application.windowInterface.WindowHandler;
 
-import javax.swing.*;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -11,7 +10,6 @@ public class SimulationHandler {
     public static int WINDOW_WIDTH;
     public static int WINDOW_HEIGHT;
     public static int BestTime;
-    //public static final int basevar = 100;
     public static LinkedBlockingQueue<Animal> animals;
     private static SimulData CurrentBestRun = new SimulData(0,0,0);
     public SimulationHandler()
@@ -52,9 +50,9 @@ public class SimulationHandler {
         // Creating thread-safe queue (FIFO) of all the animals alive on the field
         animals = new LinkedBlockingQueue<Animal>();
         // Adding base amount of animals
-        QueueAddRabbits(Data.RabbitsCount, animals, 1000, width, height);
-        QueueAddWolfs(Data.WolfsCount, animals, 3000, width, height);
-        QueueAddGrass(Data.GrassCount, animals, 100, width, height);
+        AddAnimalsRandom(Animal.TypeOfAnimal.RABBIT, Data.RabbitsCount, animals, 1000, width, height);
+        AddAnimalsRandom(Animal.TypeOfAnimal.WOLF, Data.WolfsCount, animals, 3000, width, height);
+        AddAnimalsRandom(Animal.TypeOfAnimal.GRASS, Data.GrassCount, animals, 100, width, height);
         // Main routine
         WindowHandler window = WindowHandler.CreateWindow(width,height);
         while (animals.size() != 0 && CompletedTasksCount < Integer.MAX_VALUE) // TODO: not safe cuz movescount
@@ -86,30 +84,12 @@ public class SimulationHandler {
     {
         return 1;
     }
-    private static void QueueAddRabbits(int n, LinkedBlockingQueue<Animal> animals, int baseHealth, int width, int height) // Adds to random x y coordinates
+    private static void AddAnimalsRandom(Animal.TypeOfAnimal type, int n, LinkedBlockingQueue<Animal> animals, int baseHealth, int width, int height) // Adds to random x y coordinates
     {
         Random rand = new Random();
         for (int i = 0; i < n; i++) {
             Coordinates coords = new Coordinates(rand.nextInt(width + 1), rand.nextInt(height + 1));
-            Animal.SpawnAt(Animal.TypeOfAnimal.RABBIT, animals, coords, baseHealth);
+            Animal.SpawnAt(type, animals, coords, baseHealth);
         }
     }
-    private static void QueueAddWolfs(int n, LinkedBlockingQueue<Animal> animals, int baseHealth, int width, int height) // Adds to random x y coordinates
-    {
-        Random rand = new Random();
-        for (int i = 0; i < n; i++) {
-            Coordinates coords = new Coordinates(rand.nextInt(width + 1), rand.nextInt(height + 1));
-            Animal.SpawnAt(Animal.TypeOfAnimal.WOLF, animals, coords, baseHealth);
-        }
-    }
-    private static void QueueAddGrass(int n, LinkedBlockingQueue<Animal> animals, int baseHealth, int width, int height) // Adds to random x y coordinates
-    {
-        Random rand = new Random();
-        for (int i = 0; i < n; i++) {
-            Coordinates coords = new Coordinates(rand.nextInt(width + 1), rand.nextInt(height + 1));
-            Animal.SpawnAt(Animal.TypeOfAnimal.GRASS, animals, coords, baseHealth);
-        }
-    }
-
-
 }
